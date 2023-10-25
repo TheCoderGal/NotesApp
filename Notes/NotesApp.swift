49860c36903d72d8d4e2736bbe303805a84d26b8
RohiniVaidya <rohinivaidya2310@gmail.com>
2023-10-25 18:16:28 +0530

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Rosh on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
